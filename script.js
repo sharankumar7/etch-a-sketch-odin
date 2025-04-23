e8647530b59e692ddbd922a61fdb8168e1e88ac2
Sharan kumar <bssharankumar28920@gmail.com>
2025-04-23 12:06:53 +0530

    declaring elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.querySelector("#input");
+const createBtn = document.querySelector("#create-grid");
+const clearBtn = document.querySelector("#clear-btn");
+const container = document.querySelector(".container");
